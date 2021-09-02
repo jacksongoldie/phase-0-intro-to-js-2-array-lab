cd9144c17ee9a9ce7025768f0423b1a1c02516a4
Goldie <goldiejackson25@yahoo.com>
2021-09-02 10:18:42 -0500

    commit tree d246d64571e37b98c1dd655bc87405e06aaaed58

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
+
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"];
 
-/*destructivelyAppendCat(name)
-        1) appends a cat to the end of the cats array
-      destructivelyPrependCat(name)*/
-function destructivelyAppendCat(nameOfCat){
-    cats.push(nameOfCat);
+//destructivelyAppendCat(name)
+function destructivelyAppendCat(catName){
+    cats.push(catName);
 }
 
-/*2) prepends a cat to the beginning of the cats array
-      destructivelyRemoveLastCat()*/
-function destructivelyPrependCat(nameOfCat){
-    cats.unshift(nameOfCat);
+//2)   destructivelyPrependCat(name) prepends a cat to the beginning of the cats array
+function destructivelyPrependCat(catName){
+    cats.unshift(catName);
 }
 
-/* 3) removes the last cat from the cats array
-      destructivelyRemoveFirstCat()*/
+//3)destructivelyRemoveLastCat() removes the last cat from the cats array
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}    
+
+//4)  destructivelyRemoveFirstCat()*/removes the first cat from the cats array
 function destructivelyRemoveFirstCat(){
     cats.shift();
 }
-
-
-//4) removes the first cat from the cats array
-function destructivelyRemoveLastCat(){
-    cats.pop();
-}
-
 
 /*5) appendCat(name)
         1) appends a cat to the cats array and returns a new array, leaving the cats array unchanged*/
 function appendCat(nameOfCat){
     const appendCatArray = [...cats, nameOfCat]
     //next line doesn't work because the push dot operator thing RETURNS the number of the elements in the array
-        //const appendCatArray = cats.push(nameOfCat);
+    //const appendCatArray = cats.push(nameOfCat);
     return appendCatArray;
 }
+
 
 /*6) prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
       */
@@ -47,7 +43,7 @@ function prependCat(nameOfCat){
 /*7) removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
 */
 function removeLastCat(){
-   // const removeLastCatArray = cats.splice(0, 1);  <--- is this not working bc it changes the original cats??
+   // const removeLastCatArray = cats.splice(0, 1);  <--- is this not working bc it changes the original cats?? CORRECT, yay you learned
     const removeLastCatArray = cats.slice(0, cats.length -1);
     return removeLastCatArray;
 }
